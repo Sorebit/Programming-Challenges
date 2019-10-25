a10275c12d09d2c22a7a6298b1652efffc805556
Sorebit <cmbk90@gmail.com>
2019-10-25 13:59:55 +0200

    Reformat with yapf

--- 96-dot-cross-vector/main.py
+++ 96-dot-cross-vector/main.py
@@ -1,20 +1,30 @@
 import re
 
+
 # This method assumes valid vectors of the same length
 def dot_product(a, b):
-    return sum([i*j for (i, j) in zip(a, b)])
+    return sum([i * j for (i, j) in zip(a, b)])
+
 
 # This method assumes valid vectors of length 2
 def cross_product_2(a, b):
     return cross_product_3(a + [0], b + [0])
+
+
 # This method assumes valid vectors of length 3
 def cross_product_3(a, b):
-    return [a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0]]
+    return [
+        a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ]
+
+
 # This method assumes same-length valid vectors of length 2 or 3
 def cross_product(a, b):
     if len(a) == 2:
         return cross_product_2(a, b)
     return cross_product_3(a, b)
+
 
 # Handle method input
 def read_method():
@@ -29,10 +39,13 @@ def read_method():
         elif m == "dot" or m == "d":
             return "dot"
 
+
 # Handle vectors input
 def read_vectors(method):
     while True:
-        print("\nInput two vectors of the same length (ex. 1.0 2e3 4 then .5 6 7)")
+        print(
+            "\nInput two vectors of the same length (ex. 1.0 2e3 4 then .5 6 7)"
+        )
         v = input("First> ")
         v = v.strip()
         a = re.split("\s+", v)
@@ -40,12 +53,14 @@ def read_vectors(method):
         v = input("Second> ")
         v = v.strip()
         b = re.split("\s+", v)
-        
+
         if len(a) != len(b):
             continue
         # Only support 2 and 3 dim vectors for cross product
         if method == "cross" and not (len(a) == 2 or len(a) == 3):
-            print("Error: Cross product only supports 2 and 3 dimensional vectors!")
+            print(
+                "Error: Cross product only supports 2 and 3 dimensional vectors!"
+            )
             continue
         # Dimensions as expected
         a = [float(i) for i in a]
@@ -65,6 +80,7 @@ def main():
         result = cross_product(a, b)
 
     print("\n" + method.capitalize() + " product: " + str(result))
+
 
 # Script execution
 if __name__ == "__main__":
