587b8ac747850993e444151568aedf5d1d267329
Sorebit <cmbk90@gmail.com>
2019-11-13 18:00:20 +0100

    75/Add Python quine

---
+++ 75-Quine/quine.py
@@ -0,0 +1,19 @@
+c = 34
+l = [
+    "c = 34",
+    "l = [",
+    "]",
+    "for i in range(0, 1):",
+    "    print(l[i])",
+    "for i in range(1, len(l)):",
+    "    print('    ' + chr(c) + l[i] + chr(c) + ',')",
+    "for in in range(6, len(l)):",
+    "    print(l[i])",
+    "",
+]
+for i in range(0, 2):
+    print(l[i])
+for i in range(0, len(l)):
+    print('   ' + chr(c) + l[i] + chr(c) + ',')
+for i in range(3, len(l)):
+    print(l[i])
